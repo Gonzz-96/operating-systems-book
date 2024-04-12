2e2440822b43bd43d4a33e53edfe22bb197211d5
Gonzalo Campos <gonzalocampose1@gmail.com>
2024-04-12 22:57:22 +0000

    Add fork example.

---
+++ chapter3/fork/fork.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    
+    pid_t pid;
+
+    
+    pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed");
+        return 1;
+    } else if (pid == 0) {
+        execlp("/bin/ls", "ls", NULL);
+        printf("\n");
+    } else {
+        wait(NULL);
+        printf("Child complete\n");
+    }
+    
+    return 0;
+}
+
