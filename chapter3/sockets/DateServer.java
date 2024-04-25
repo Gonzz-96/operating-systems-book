e1db2bc26b796ce9643467fe0c80c1b49e40f9fb
Gonzalo Campos <gonzalocampose1@gmail.com>
2024-04-25 22:03:00 +0000

    Java sockets.

---
+++ chapter3/sockets/DateServer.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+
+public class DateServer {
+
+    public static void main(String[] args) {
+        try  {
+            ServerSocket sock = new ServerSocket(6013);
+
+            while (true) {
+                Socket client = sock.accept();
+
+                PrintWriter pout = new
+                    PrintWriter(client.getOutputStream(), true);
+
+                System.out.println("Client request accepted");
+                pout.println(new java.util.Date().toString());
+
+                client.close();
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+}
+
