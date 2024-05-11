b6a64fc58af3c86ca569f8781c1c40606b798d2c
Gonzalo Campos <gonzalocampose1@gmail.com>
2024-05-11 18:40:10 +0000

    Add unbuffered version

--- the-c-programming-language/echo/echo_syscalls.c
+++ the-c-programming-language/echo/echo_syscalls.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "getchar.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "usage: ./echo <buffered|unbuffered>\n");
+        return 1;
+    }
 
     char buf[BUFSIZ];
     int n;
@@ -11,7 +17,23 @@ int main() {
     // when using this version of getchar, stdin
     // gets empty since it's completely read by the
     // buffered_getchar function.
-    printf("%c\n", (char)buffered_getchar());
+    //
+    // Advantages: a single syscall is needed
+    // Disadvantages: stding is emptied
+    if (strcmp(argv[1], "buffered") == 0) {
+        printf("%c\n", (char)buffered_getchar());
+    }
+
+    // in this case, the unbuffered version reads one
+    // character at a time, leaving the rest to the
+    // upcoming while loop
+    //
+    // Advantages: the buffer is read one char at a time
+    // Disadvantages: every call is a system call,
+    // requiring the OS to make multiple context switches
+    if (strcmp(argv[1], "unbuffered") == 0) {
+        printf("%c\n", (char)unbuffered_getchar());
+    }
 
     while ((n = read(0, buf, BUFSIZ)) > 0 ) {
         write(1, buf, n);
--- the-c-programming-language/echo/getchar.h
+++ the-c-programming-language/echo/getchar.h
@@ -15,3 +15,10 @@ int buffered_getchar() {
         : EOF;
 }
 
+int unbuffered_getchar() {
+    char c;
+    return (read(0, &c, 1) == 1)
+        ? (unsigned char) c
+        : EOF;
+}
+
